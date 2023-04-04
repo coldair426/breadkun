bb72fb8712816f1ed4c4ecff10207a5a4d6385b7
chanki <coldair426@gmail.com>
2023-04-05 02:09:57 +0900

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.png';
-import './App.css';
+import { Reset } from 'styled-reset';
+import './style/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Reset />
+      <div>안녕하세요. 빵돌입니다.</div>
+    </React.Fragment>
   );
 }
 
