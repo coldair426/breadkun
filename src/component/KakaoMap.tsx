0b93285b52d1ab92cde60e4ff3955f1611f5ca1f
chanki <coldair426@gmail.com>
2023-04-07 20:06:16 +0900

    Revert "pakage inst"
    This reverts commit 5b3c6ea0eecbe49cb38b2bcc2cca2b87b46c54d1.

--- src/component/KakaoMap.tsx
+++ src/component/KakaoMap.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 
 declare global {
   interface Window {
@@ -6,19 +6,21 @@ declare global {
   }
 }
 
-function KakaoMap() {
-  useEffect(() => {
-    let container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    let options = {
-      //지도를 생성할 때 필요한 기본 옵션
-      center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 3, //지도의 레벨(확대, 축소 정도)
-    };
+const options = {
+  //지도를 생성할 때 필요한 기본 옵션
+  center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+  level: 3, //지도의 레벨(확대, 축소 정도)
+};
 
-    let map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+function KakaoMap() {
+  const container = useRef(null); //지도를 담을 영역의 DOM 레퍼런스
+
+  useEffect(() => {
+    new window.kakao.maps.Map(container.current, options); //지도 생성 및 객체 리턴
+    return () => {};
   }, []);
 
-  return <div id='map' style={{ width: '500px', height: '400px' }}></div>;
+  return <div className='map' style={{ width: '500px', height: '500px' }} ref={container}></div>;
 }
 
 export default KakaoMap;
