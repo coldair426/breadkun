c74826cbbd957b9c889e785db6533334cb0bbd47
chanki <coldair426@gmail.com>
2023-04-10 04:16:59 +0900

    test2

--- src/component/KakaoMap.tsx
+++ src/component/KakaoMap.tsx
@@ -10,24 +10,25 @@ declare global {
   }
 }
 
-function KakaoMap() {
+function KakaoMap({ latitude, longitude }: { latitude: number; longitude: number }) {
   const mapRef = useRef(null);
-
+  // 지도 생성(초기화)
   useEffect(() => {
     const container = mapRef.current; // return 값 DOM
     const options = {
-      center: new window.kakao.maps.LatLng(37.420125, 127.126665),
-      level: 3,
+      center: new window.kakao.maps.LatLng(latitude, longitude),
+      draggable: false, // 이동, 확대, 축소 금지
+      level: 4, // 지도 확대 레벨
     };
-    new window.kakao.maps.Map(container, options);
-  }, []);
+    const map = new window.kakao.maps.Map(container, options); // 지도생성
+  }, [latitude, longitude]);
 
   return <div ref={mapRef} className={ks('map')} />;
 }
 
-KakaoMap.defaultProps = {
-  size: 'medium',
-  color: 'blue',
-};
+// KakaoMap.defaultProps = {
+//   size: 'medium',
+//   color: 'blue',
+// };
 
 export default KakaoMap;
--- src/page/Bus.tsx
+++ src/page/Bus.tsx
@@ -7,7 +7,7 @@ import KakaoMap from '../component/KakaoMap';
 const bs = classNames.bind(styles);
 
 function Bus() {
-  const [latLong, setLatLong] = useState({ latitude: 0, longitude: 0 });
+  const [latLong, setLatLong] = useState({ latitude: 37.756540912483615, longitude: 127.63819968679633 });
   useEffect(() => {
     // 페이지 최상단으로 스크롤링
     window.scrollTo(0, 0);
@@ -23,13 +23,13 @@ function Bus() {
     <div className={bs('bus')}>
       <Title letter='출퇴근 버스 정보' imgSrc='/icon/bus-title-icon.png' imgH='22px' />
       <div className={bs('bus__body')}>
+        <KakaoMap latitude={latLong.latitude} longitude={latLong.longitude} />
         <div className={bs('bus__block1')}>
           <div className={bs('bus__block1--title')}>현재 위치에서 남은 시간</div>
           <div>latitude: {latLong.latitude}</div>
           <div>longitude: {latLong.longitude}</div>
           <button onClick={() => updateLocation()}>위치 새로고침</button>
         </div>
-        <KakaoMap />
       </div>
     </div>
   );
