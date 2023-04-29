268b091e4aa56479add831a638d4c9ba02225345
chanki <coldair426@gmail.com>
2023-04-30 06:01:36 +0900

    open api connection, key value env processing

--- src/page/Bus.tsx
+++ src/page/Bus.tsx
@@ -13,7 +13,7 @@ const bs = classNames.bind(styles);
 function Bus({ setMenuBox }: { setMenuBox: React.Dispatch<React.SetStateAction<boolean>> }) {
   let { destination } = useParams(); // URL parameter
   const [selectedValue, setSelectedValue] = useState(destination || localStorage.getItem('recentDestination') || '강변1'); // URL parameter 노선 or 로컬스토리지 or "강변1"(기본)
-  const [latLong, setLatLong] = useState({ latitude: 37.756540912483615, longitude: 127.63819968679633 }); // 현재위치 정보 lat&log
+  const [latLong, setLatLong] = useState({ latitude: 37.756540912483615, longitude: 127.63819968679633 }); // 현재위치 정보 lat&log, 기본값 더존 강촌캠
   const [address, setAddress] = useState({ region_1depth_name: '강원', region_2depth_name: '춘천시', region_3depth_name: '남산면' }); // 현재위치 정보 도로명 주소
   const [arrivalTime, setArrivalTime] = useState({ mainbox: '-----', time: '', ampm: '', remainingTime: '', remainingText: '' }); // 도착시간
   const [notification, setNotification] = useState(false); // 스낵바
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styles from '../style/Home.module.scss';
 import classNames from 'classnames/bind';
-// import axios from 'axios';
+import axios from 'axios';
 
 const hs = classNames.bind(styles);
 
@@ -14,18 +14,29 @@ function Home({ setMenuBox }: { setMenuBox: React.Dispatch<React.SetStateAction<
   useEffect(() => {
     window.scrollTo(0, 0); // 페이지 최상단으로 스크롤링
   }, []);
-  // // 미세먼지, 초미세먼지 받아오기(async & await)
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     try {
-  //       const result = await axios.get('');
-  //       console.log(result);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   }
-  //   fetchData();
-  // }, []);
+  // 미세먼지, 초미세먼지 받아오기(async & await)
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        if (company === '강촌') {
+          // 강촌캠 기상 상태 조회
+          const resultK = await axios.get(
+            `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=가평&ver=1.4&dataTerm=daily&pageNo=1&numOfRows=1&returnType=json&serviceKey=${process.env.REACT_APP_AIRKOREA_DUST_API}`
+          );
+          console.log(resultK.data.response.body.items[0]);
+        } else {
+          // 을지타워 기상 상태 조회
+          const resultS = await axios.get(
+            `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=중구&ver=1.4&dataTerm=daily&pageNo=1&numOfRows=1&returnType=json&serviceKey=${process.env.REACT_APP_AIRKOREA_DUST_API}`
+          );
+          console.log(resultS.data.response.body.items[0]);
+        }
+      } catch (error) {
+        console.log('error');
+      }
+    }
+    fetchData();
+  }, [company]);
   useEffect(() => {
     localStorage.setItem('recentCompany', company); // 로컬 스토리지 업데이트
   }, [company]);
