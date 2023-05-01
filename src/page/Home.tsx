e3b06cae25a80dbf719dd707c2a152e3c9920a71
chanki <coldair426@gmail.com>
2023-05-01 19:55:38 +0900

    open api connect

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styles from '../style/Home.module.scss';
 import classNames from 'classnames/bind';
-// import axios from 'axios';
+import axios from 'axios';
 
 const hs = classNames.bind(styles);
 
@@ -14,30 +14,54 @@ function Home({ setMenuBox }: { setMenuBox: React.Dispatch<React.SetStateAction<
   useEffect(() => {
     window.scrollTo(0, 0); // 페이지 최상단으로 스크롤링
   }, []);
-  // 미세먼지, 초미세먼지 받아오기(async & await)
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     try {
-  //       if (company === '강촌') {
-  //         // 강촌캠 기상 상태 조회
-  //         const resultK = await axios.get(
-  //           `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=가평&ver=1.4&dataTerm=daily&pageNo=1&numOfRows=1&returnType=json&serviceKey=${process.env.REACT_APP_AIRKOREA_DUST_API}`
-  //         );
-  //         console.log(resultK.data.response.body.items[0]);
-  //       } else {
-  //         // 을지타워 기상 상태 조회
-  //         const resultS = await axios.get(
-  //           `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=중구&ver=1.4&dataTerm=daily&pageNo=1&numOfRows=1&returnType=json&serviceKey=${process.env.REACT_APP_AIRKOREA_DUST_API}`
-  //         );
-  //         console.log(resultS.data.response.body.items[0]);
-  //       }
-  //     } catch (error) {
-  //       console.log('에러입니다.');
-  //       console.log(error);
-  //     }
-  //   }
-  //   fetchData();
-  // }, [company]);
+  // 에어코리아 미세먼지, 초미세먼지
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        if (company === '강촌') {
+          // 강촌캠 기상 상태 조회
+          const resultK = await axios.get(
+            `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=가평&ver=1.4&dataTerm=daily&pageNo=1&numOfRows=1&returnType=json&serviceKey=${process.env.REACT_APP_PUBLIC_OPEN_API_ENCODING_KEY}`
+          );
+          console.log(resultK.data.response.body.items[0]);
+        } else {
+          // 을지타워 기상 상태 조회
+          const resultS = await axios.get(
+            `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=중구&ver=1.4&dataTerm=daily&pageNo=1&numOfRows=1&returnType=json&serviceKey=${process.env.REACT_APP_PUBLIC_OPEN_API_ENCODING_KEY}`
+          );
+          console.log(resultS.data.response.body.items[0]);
+        }
+      } catch (error) {
+        console.log('에러입니다.');
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, [company]);
+  // 기상청 날씨
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        if (company === '강촌') {
+          // 강촌캠 날씨 조회
+          const resultK = await axios.get(
+            `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey=${process.env.REACT_APP_PUBLIC_OPEN_API_ENCODING_KEY}&numOfRows=100&pageNo=1&dataType=json&base_date=20230501&base_time=1900&nx=71&ny=132`
+          );
+          console.log(resultK.data.response.body.items.item);
+        } else {
+          // 을지타워 날씨 조회
+          const resultS = await axios.get(
+            `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey=${process.env.REACT_APP_PUBLIC_OPEN_API_ENCODING_KEY}&numOfRows=100&pageNo=1&dataType=json&base_date=20230501&base_time=1900&nx=60&ny=127`
+          );
+          console.log(resultS.data.response.body.items.item);
+        }
+      } catch (error) {
+        console.log('에러입니다.');
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, [company]);
   useEffect(() => {
     localStorage.setItem('recentCompany', company); // 로컬 스토리지 업데이트
   }, [company]);
