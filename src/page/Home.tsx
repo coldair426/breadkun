c2415e1004e4e41d3cb6a3142c90c8238b717b64
chanki <coldair426@gmail.com>
2023-05-08 02:55:56 +0900

    weather API edit

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -10,7 +10,7 @@ function Home({ setMenuBox }: { setMenuBox: React.Dispatch<React.SetStateAction<
   const [company, setCompany] = useState(localStorage.getItem('recentCompany') || '강촌'); // 강촌, 을지
   const [notification, setNotification] = useState(false);
   const [dust, setDust] = useState({ dataTime: '--', stationName: '--', pm10Level: '조회중', pm25Level: '조회중', pm10Value: '--', pm25Value: '--' });
-  // const [weather, setWeather] = useState({ dataTime: '--', stationName: '--', pm10Level: '조회중', pm25Level: '조회중', pm10Value: '--', pm25Value: '--' });
+  const [weather, setWeather] = useState([]);
 
   // 회사를 드롭다운에 따라 업데이트하는 함수
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -26,34 +26,33 @@ function Home({ setMenuBox }: { setMenuBox: React.Dispatch<React.SetStateAction<
   // 에어코리아 미세먼지, 초미세먼지
   useEffect(() => {
     localStorage.setItem('recentCompany', company); // 로컬 스토리지 업데이트
-    // const currentDate = new Date(); // 현재 날짜
-    // let date: string;
-    // let time: string;
-    // if (currentDate.getHours() === 0) {
-    //   // 00시에는 하루전날 23시에 조회.
-    //   date =
-    //     `${currentDate.getFullYear()}` +
-    //     (currentDate.getMonth() + 1 > 9 ? `${currentDate.getMonth() + 1}` : `0${currentDate.getMonth() + 1}`) +
-    //     (currentDate.getDay() - 1 > 9 ? `${currentDate.getDay() - 1}` : `0${currentDate.getDay() - 1}`);
-    //   time = '2300';
-    // } else {
-    //   // yyyymmdd 형식으로 수정
-    //   date =
-    //     `${currentDate.getFullYear()}` +
-    //     (currentDate.getMonth() + 1 > 9 ? `${currentDate.getMonth() + 1}` : `0${currentDate.getMonth() + 1}`) +
-    //     (currentDate.getDay() > 9 ? `${currentDate.getDay()}` : `0${currentDate.getDay()}`);
-    //   // hhmm 형식으로 수정 => 단, 분단위(mm)는 무조건 00분 이고 시간은 현재 시간에서 -1(한시간전) 조회
-    //   time = (currentDate.getHours() - 1 > 9 ? `${currentDate.getHours() - 1}` : `0${currentDate.getHours() - 1}`) + '00';
-    // }
+    const now = new Date(); // 현재 날짜
+    const previousTime = new Date(now);
+    previousTime.setHours(previousTime.getHours() - 1);
+    const year = previousTime.getFullYear();
+    const month = (previousTime.getMonth() + 1).toString().padStart(2, '0');
+    const day = previousTime.getDate().toString().padStart(2, '0');
+    const time = previousTime.getHours().toString().padStart(2, '0').padEnd(4, '0');
+    const currentDate = `${year}${month}${day}`;
+    const currentTime = `${time}`;
+
     async function fetchData() {
       setNotification(true);
       try {
         // 미세먼지 조회
-        const dustResult = await axios.get(
+        const dustPromise = axios.get(
           `https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=${
             company === '강촌' ? '가평' : '중구'
           }&ver=1.4&dataTerm=daily&pageNo=1&numOfRows=1&returnType=json&serviceKey=${process.env.REACT_APP_PUBLIC_OPEN_API_ENCODING_KEY}`
         );
+        // 날씨 조회
+        const weatherPromise = axios.get(
+          `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey=${
+            process.env.REACT_APP_PUBLIC_OPEN_API_ENCODING_KEY
+          }&numOfRows=100&pageNo=1&dataType=json&base_date=${currentDate}&base_time=${currentTime}&nx=${company === '강촌' ? 71 : 60}&ny=${company === '강촌' ? 132 : 127}`
+        );
+        const [dustResult, weatherResult] = await axios.all([dustPromise, weatherPromise]); // 병렬통신
+        // 미세먼지, 초미세먼지
         const { dataTime, stationName, pm10Value, pm25Value } = dustResult.data.response.body.items[0];
         let pm10Level = '';
         let pm25Level = '';
@@ -78,13 +77,9 @@ function Home({ setMenuBox }: { setMenuBox: React.Dispatch<React.SetStateAction<
           pm25Level = '최악';
         }
         setDust({ dataTime, stationName, pm10Level, pm25Level, pm10Value, pm25Value });
-        // 날씨 조회
-        // const weatherResult = await axios.get(
-        //   `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey=${
-        //     process.env.REACT_APP_PUBLIC_OPEN_API_ENCODING_KEY
-        //   }&numOfRows=100&pageNo=1&dataType=json&base_date=${date}&base_time=${time}&nx=${company === '강촌' ? 71 : 60}&ny=${company === '강촌' ? 132 : 127}`
-        // );
-        // setWeather(weatherResult.data.response.body.items.item);
+        // 날씨
+        setWeather(weatherResult.data.response.body.items.item);
+        console.log(weatherResult.data.response.body.items.item);
         setNotification(false);
       } catch (error) {
         setNotification(false);
