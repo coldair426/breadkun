0ea169293ca55e9e827e8a5d2e70fa5aaac7d242
chanki <coldair426@gmail.com>
2023-07-31 10:57:23 +0900

    Revert "React snap"
    This reverts commit 5a58aac3811fcf23092af3f514f3568d459a14ec.

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,21 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import { hydrate, render } from 'react-dom';
 
-const rootElement = document.getElementById('root') as HTMLElement;
-
-if (rootElement.hasChildNodes()) {
-  hydrate(
-    <React.StrictMode>
-      <BrowserRouter>
-        <HelmetProvider>
-          <App />
-        </HelmetProvider>
-      </BrowserRouter>
-    </React.StrictMode>,
-    rootElement
-  );
-} else {
-  render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <HelmetProvider>
-          <App />
-        </HelmetProvider>
-      </BrowserRouter>
-    </React.StrictMode>,
-    rootElement
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
 serviceWorkerRegistration.register();
 reportWebVitals();
