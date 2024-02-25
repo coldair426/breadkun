256bbd130bd71a8a89630a15c7793e55513f575a
aesop0817 <aesop0817@douzone.com>
2024-02-25 15:45:25 +0900

    refactor: meal-api 분리

--- src/apis/weather/weather-api.ts
+++ src/apis/weather/weather-api.ts
@@ -1,57 +1,8 @@
 import axios from "axios";
+import { WeatherReturn } from "types/home";
 import {addOrSubDays, formatDate, formatTime} from "../../utils/dates";
 
 const cancelTokenSource = axios.CancelToken.source(); // 요청 취소 토큰
-
-export interface WeatherReturn {
-    baseDate: string;
-    baseTime: string;
-    category: string;
-    fcstDate: string;
-    fcstTime: string;
-    fcstValue: string;
-    nx: number;
-    ny: number;
-}
-
-const calculateDateAndTime = () => {
-    const now = new Date(); // 현재 날짜
-    const yesterday = new Date(now); // 어제 날짜
-
-// 달과 연도 넘어갈때 버그 방지 처리
-    yesterday.getDate() > now.getDate() && yesterday.setMonth(yesterday.getMonth() - 1); // 현재 날짜와 어제 날짜의 일(day)이 다른 경우 이전 달로 설정
-
-// 현재 날짜 및 시간 포맷
-    const currentDate = formatDate(now)
-    const currentTime = formatTime(now)
-
-// 어제 날짜 포맷
-    const yesterdayDate = formatDate(addOrSubDays('sub', now, 1))
-
-    let baseDate = currentDate; // 조회날짜
-    let baseTime = ''; // 조회시간
-// '0200', '0500', '0800', '1100', '1400', '1700', '2000', '2300' 기상청 API 일 8회 업데이트 시간 1시간 후에 조회.
-    if (currentTime < 180) {
-        baseDate = yesterdayDate;
-        baseTime = '2300';
-    } else if (currentTime < 360) {
-        baseTime = '0200';
-    } else if (currentTime < 540) {
-        baseTime = '0500';
-    } else if (currentTime < 720) {
-        baseTime = '0800';
-    } else if (currentTime < 900) {
-        baseTime = '1100';
-    } else if (currentTime < 1080) {
-        baseTime = '1400';
-    } else if (currentTime < 1260) {
-        baseTime = '1700';
-    } else {
-        baseTime = '2000';
-    }
-
-    return {baseDate, baseTime}
-}
 
 
 export const fetchWeatherData = async (company: string) => {
@@ -63,11 +14,11 @@ export const fetchWeatherData = async (company: string) => {
     yesterday.getDate() > now.getDate() && yesterday.setMonth(yesterday.getMonth() - 1); // 현재 날짜와 어제 날짜의 일(day)이 다른 경우 이전 달로 설정
 
 // 현재 날짜 및 시간 포맷
-    const currentDate = formatDate(now)
+    const currentDate = formatDate(now, 1)
     const currentTime = formatTime(now)
 
 // 어제 날짜 포맷
-    const yesterdayDate = formatDate(addOrSubDays('sub', now, 1))
+    const yesterdayDate = formatDate(addOrSubDays('sub', now, 1),1)
 
     let baseDate = currentDate; // 조회날짜
     let baseTime = ''; // 조회시간
--- src/utils/dates/index.ts
+++ src/utils/dates/index.ts
@@ -16,10 +16,40 @@ export function addOrSubDays(type:string, d: Date | null, days: number) {
 }
 
 //Date 형식을 YYYYMMDD 형식으로 변경
-export const formatDate = (date: Date) => {
-    return date.getFullYear() + ('00' + (date.getMonth() + 1)).slice(-2) + ('00' + date.getDate()).slice(-2);
+export const formatDate = (date: Date, type:number) => {
+    if (date === null) return '';
+    if(type===1){
+        return date.getFullYear() + ('00' + (date.getMonth() + 1)).slice(-2) + ('00' + date.getDate()).slice(-2);
+    }
+    if(type===2){
+        const options = { month: '2-digit' as const, day: '2-digit' as const, weekday: 'short' as const };
+        const formattedDate = new Intl.DateTimeFormat('ko-KR', options).format(date);
+        return formattedDate.replace(/\.\s(?=\()/g, ' ')
+    }
+    return ''
 };
 
 export const formatTime=(date:Date)=>{
     return date.getHours()*60+date.getMinutes();
 }
+
+export const dayNumToSpell = (value: number): string => {
+    switch (value) {
+        case 0:
+            return '월';
+        case 1:
+            return '화';
+        case 2:
+            return '수';
+        case 3:
+            return '목';
+        case 4:
+            return '금';
+        case 5:
+            return '토';
+        case 6:
+            return '일';
+        default:
+            return '월';
+    }
+};
